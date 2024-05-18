8113c4cdfa207f1060e9f92e701585353e210df0
Hardelz <135560555+Hardelzs@users.noreply.github.com>
2024-05-19 00:18:05 +0100

    created things

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -4,59 +4,67 @@ import React from "react";
 import { TypeAnimation } from "react-type-animation";
 
 import {
-  FaTwitter,
-  FaFacebook,
-  FaInstagram,
-  FaLinkedinIn,
+  FaTwitter,
+  FaFacebook,
+  FaInstagram,
+  FaLinkedinIn,
 } from "react-icons/fa";
 
-
 const Main = () => {
-  // const [login, register] = useKindeAuth();
-  return (
-    <div id="main">
-       
-      <img
-        className="w-full h-screen object-cover object-left scale-x-[-1]"
-        src=""
-        alt=""
-      />
-      <div className="w-full h-screen absolute top-0 left-0 bg-white/50">
-        <div className="max-w-[700px] m-auto h-full w-full flex flex-col justify-center lg:items-start items-center">
-          <h1 className="sm:text-5xl text-4xl font-bold text-gray-800">
-            I am Adewale Ezekiel
-          </h1>
-          <h2 className="flex sm:text-3xl text-2xl pt-4 text-gray-800">
-            I am a
-            <TypeAnimation
-              sequence={[
-                "Developer",
-                2000, // wait 1s before replacing "Mice" with "Hamsters"
-                "Coder",
-                2000,
-                "Tech Enthusiast",
-                2000,
-                "Coffee Lover",
-                2000,
-              ]}
-              wrapper="div"
-              speed={50}
-              cursor={true}
-              style={{ fontSize: "1em", paddingLeft: "5px" }}
-              repeat={Infinity}
-            />
-          </h2>
-          <div className="flex justify-between pt-6 max-w-[200px] w-full">
-            <FaTwitter className="cursor-pointer" size={20}  />
-            
-            <FaFacebook className="cursor-pointer" size={20} />
-            <FaInstagram className="cursor-pointer" size={20} />
-            <FaLinkedinIn className="cursor-pointer" size={20} />
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+  // const [login, register] = useKindeAuth();
+  return (
+    <div id="main">
+            {" "}
+      <img
+        className="w-full h-screen object-cover object-left scale-x-[-1]"
+        src=""
+        alt=""
+      />
+        {" "}
+      <div className="w-full h-screen absolute top-0 left-0 bg-white/50">
+           {" "}
+        <div className="max-w-[700px] m-auto h-full w-full flex flex-col justify-center lg:items-start items-center">
+              {" "}
+          <h1 className="sm:text-5xl text-4xl font-bold text-gray-800">
+                  I'm Adewale Ezekiel     {" "}
+          </h1>
+              {" "}
+          <h2 className="flex sm:text-3xl text-2xl pt-4 text-gray-800">
+                  I am a      {" "}
+            <TypeAnimation
+              sequence={[
+                "Developer",
+                2000, // wait 1s before replacing "Mice" with "Hamsters"
+                "Coder",
+                2000,
+                "Tech Enthusiast",
+                2000,
+                "Coffee Lover",
+                2000,
+              ]}
+              wrapper="div"
+              speed={50}
+              cursor={true}
+              style={{ fontSize: "1em", paddingLeft: "5px" }}
+              repeat={Infinity}
+            />
+                {" "}
+          </h2>
+              {" "}
+          <div className="flex justify-between pt-6 max-w-[200px] w-full">
+            <FaTwitter className="cursor-pointer" size={20} />
+                  <FaFacebook className="cursor-pointer" size={20} />
+                  <FaInstagram className="cursor-pointer" size={20} />
+                  <FaLinkedinIn className="cursor-pointer" size={20} /> 
+              {" "}
+          </div>
+             {" "}
+        </div>
+          {" "}
+      </div>
+       {" "}
+    </div>
+  );
 };
 
 export default Main;
--- src/components/Sidenav.jsx
+++ src/components/Sidenav.jsx
@@ -21,37 +21,42 @@ const Sidenav = () => {
         onClick={handleNav}
         className="absolute top-4 right-4 z-[99] md:hidden"
       />
-      {nav ? 
+      {nav ? (
         <div className="fixed w-full h-screen bg-white/90 flex flex-col justify-center items-center z-20">
-          <a onClick={handleNav}
+          <a
+            onClick={handleNav}
             href="#main"
             className="w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200"
           >
             <AiOutlineHome size={20} />
             <span className="pl-4">Home</span>
           </a>
-          <a onClick={handleNav}
+          <a
+            onClick={handleNav}
             href="#work"
             className="w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200"
           >
             <GrProjects size={20} />
             <span className="pl-4">Work</span>
           </a>
-          <a onClick={handleNav}
+          <a
+            onClick={handleNav}
             href="#projects"
             className="w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200"
           >
             <AiOutlineProject size={20} />
             <span className="pl-4">Projects</span>
           </a>
-          <a onClick={handleNav}
+          <a
+            onClick={handleNav}
             href="#main"
             className="w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200"
           >
             <BsPerson size={20} />
             <span className="pl-4">Resume</span>
           </a>
-          <a onClick={handleNav}
+          <a
+            onClick={handleNav}
             href="#contact"
             className="w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200"
           >
@@ -59,7 +64,7 @@ const Sidenav = () => {
             <span className="pl-4">Contact</span>
           </a>
         </div>
-       : (
+      ) : (
         ""
       )}
       <div className="md:block hidden fixed top-[25%] z-10">
