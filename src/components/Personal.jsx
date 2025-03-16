6b2b052bfa4643ec71834611a16c1aa851b24a49
Hardelz <135560555+Hardelzs@users.noreply.github.com>
2025-03-16 23:14:29 +0100

    Resume page

--- src/components/Personal.jsx
+++ src/components/Personal.jsx
@@ -3,7 +3,7 @@
 const Personal = () => {
   return (
     <div id="personal" className="max-w-[1040px] m-auto md:pl-20 p-4 py-16">
-        <div className="bg-[#001b5e] h-32 text-white flex text-center items-center justify-center rounded-md">personal</div>
+        <div className="bg-[#001b5e] h-12 text-white flex text-center items-center justify-center rounded-md">Resume</div>
     </div>
   )
 }
